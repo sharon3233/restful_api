d877bdbc29601fdd51ff5051e249cfdeca94f29d
Sharon Kindles <shalaskin29@gmail.com>
2021-11-05 08:47:36 -0500

    basicAut

--- rest/app.js
+++ rest/app.js
@@ -10,6 +10,62 @@ const app = express();
 // specify out request bodies are json
 app.use(express.json());
 
+app.use(basicAuth({
+  authorizer : dbAuthorizer,
+  authorizeAsync: true,
+  unauthorizedResponse : () => 'Wrong password'
+}))
+async function dbAuthorizer(username, password, callback) {
+  try {
+    const user = await User.findOne({where : {name : username}})
+    let isValid = (user != null) ? await bcrypt.compare(password, user.password) : false;
+    callback(null, isValid)
+  } catch(err) {
+    console.log("ALERT ALERT AN ERROR", err)
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//basic auth needs a config object
+// app.use(basicAuth({
+//   authorizer : dbAuthorizer, //custom authorizer fn
+//   authorizeAsync: true, //allow our authorizer to be async
+//   unauthorizedResponse : () => 'None shall pass!'
+// }))
+
+// //compares username + password with what's in the database
+// // Returns boolean indicating if password matches
+// async function dbAuthorizer(username, password, callback){
+//   try {
+//     // get user from DB
+//     const user = await User.findOne({where : {name : username}})
+//     // isValid == true if user exists and passwords match, false if no user or passwords don't match
+//     let isValid = (user != null) ? await bcrypt.compare(password, user.password) : false;
+//     callback(null, isValid); //callback expects null as first argument
+//   } catch(err) {
+//     console.log("OH NO AN ERROR!", err)
+//     callback(null, false);
+//   }
+// }
+
 app.get('/', (req, res) => {
   res.send('<h1>RESTful API Assignment</h1>')
 })
